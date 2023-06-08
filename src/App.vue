3098de890741647f6c85adf128d598f957c36e2f
Kyon <1260328617@qq.com>
2023-06-08 13:18:38 +0800

    完成底部导航栏部署

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,38 @@
 <template>
+  <van-button type="primary">主要按钮</van-button>
+  <van-tabbar v-model="active">
+    <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+    <van-tabbar-item icon="search">标签</van-tabbar-item>
+    <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+    <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+  </van-tabbar>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
 </template>
+
+<script lang="ts">
+import { Button, Tabbar, TabbarItem } from 'vant'
+import { defineComponent } from 'vue';
+import { ref } from 'vue';
+
+export default defineComponent({
+  components: {
+    'van-button': Button,
+    'van-tabbar': Tabbar,
+    'van-tabbar-item': TabbarItem
+  },
+  setup() {
+    const active = ref(0);
+
+    return {
+      active,
+    };
+  },
+})
+</script>
 
 <style lang="less">
 #app {
@@ -14,7 +42,11 @@
   text-align: center;
   color: #2c3e50;
 }
-
+.van-tabbar {
+  position: fixed;
+  bottom: 0;
+  widows: 100%;
+}
 nav {
   padding: 30px;
 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { Button, Tabbar, TabbarItem } from 'vant'
 import router from './router'
 import store from './store'
+import 'vant/lib/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(Button).use(Tabbar).use(TabbarItem).use(store).use(router).mount('#app')
